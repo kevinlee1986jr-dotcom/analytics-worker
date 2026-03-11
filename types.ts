deaa332c2d1573aef82a79a97788f069bef519e4
kevinlee1986jr-dotcom <255597627+kevinlee1986jr-dotcom@users.noreply.github.com>
2026-03-11 17:18:24 +1000

    Update `.gitignore`

--- types.ts
+++ types.ts
@@ -5,3 +5,10 @@ interface ApiResponse<T> {
   statusCode: number;
 }
 
+
+export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+  if (val === undefined || val === null) {
+    throw new Error(`Expected 'val' to be defined, but received ${val}`);
+  }
+}
+
